9028272b03c8591c45123ff5aae70c9a3e1c5b63
Maayan Peleg <maayan.peleg87@gmail.com>
2023-03-04 11:29:01 +0000

    Added Added repo track

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import schedule
 import time
 from datetime import datetime
+from git import Repo
 
+repo = Repo(".")
+
+assert not repo.bare
 
 def job():
     with open(f'cmd-{datetime.now().strftime("%d%m%y")}.log', 'a') as log:
+        if repo.is_dirty():
+            print(repo.untracked_files)
         log.write(f'Ran Command Job At {datetime.now().strftime("%H-%M-%S")}\n')
 
-schedule.every().minute.do(job)
+schedule.every(0.1).minutes.do(job)
 
 while True:
     schedule.run_pending()
