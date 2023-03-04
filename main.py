4a0779b15ddfa61fc505e286d2232de2de0f7bd2
Maayan Peleg <maayan.peleg87@gmail.com>
2023-03-04 11:19:36 +0000

    Added GitPython

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 def job():
     with open(f'cmd-{datetime.now().strftime("%d%m%y")}.log', 'a') as log:
-        log.write(f'Ran Command Job At {datetime.now().strftime("%H%M%S")}\n')
+        log.write(f'Ran Command Job At {datetime.now().strftime("%H-%M-%S")}\n')
 
-schedule.every(0.1).minutes.do(job)
+schedule.every().minute.do(job)
 
 while True:
     schedule.run_pending()
